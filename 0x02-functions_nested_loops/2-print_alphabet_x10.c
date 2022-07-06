5c02a487345f6bcda5af37bf9128a932a4175a50
Geoffy1 <geojogu@gmail.com>
2022-07-06 05:21:18 -0700

    my second commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in small leters
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i;
+
+for (i = 0 ; i < 10; i++)
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+i++;
+}
