635c779cd2ea4f4bff8978b19c3cb285bb141828
Geoffy1 <geojogu@gmail.com>
2022-07-07 15:24:38 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,24 +14,27 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
-else if
+else if (i % 3 == 0 && i % 5 == 0)
 {
-(i % 3 == 0 && i % 5 == 0)
+printf(" FizzBuzz");
+}
+else if (i == 1)
 {
-printf("FizzBuzz ");
+printf("%d", i);
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
 }
 printf("\n");
+
 return (0);
 }
-}
+
