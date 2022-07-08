c64432ead5fcfcb414dc80a05a3b1f7e775d119e
Geoffy1 <geojogu@gmail.com>
2022-07-07 18:38:40 -0700

    my commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,23 +1,47 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
-/**
- * main- assign a random number to the variable n each time it is executed
- *  print whether the number stored in the variable n is positive or negative
- * Return: Always 0(Success)
- */
-int main(void)
-{
-int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%d is positive\n", n);
-else if (n == 0)
-	printf("%d is zero\n", n);
-else if (n < 0)
-	printf("%d is negative\n", n);
-return (0);
+
+/**
+
+ * main - This function prints the status of a given number, +ive or -ive
+
+ * Return: Always zero
+
+ */
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+
+
+	if (n > 0)
+
+		printf("%d%s", n, " is positive\n");
+
+	else if (n < 0)
+
+		printf("%d%s", n, " is negative\n");
+
+	else
+
+		printf("%d%s", n, " is zero\n");
+
+
+
+	return (0);
+
 }
