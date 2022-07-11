e319c3b0af7e31c63c98fa1aed54beab679d919c
Geoffy1 <geojogu@gmail.com>
2022-07-11 12:25:23 -0700

    5 and 6

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,20 +7,23 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len;
+	int i, len1, len2;
 
-	len = 0;
-	while (s[len] != '\0')
+	len1 = 0;
+	len2 = 0;
+
+	while (s[len1] != '\0')
 	{
-		len++;
+		len1++;
 	}
 
-	len--;
+	len2 = len1 - 1;
 
-	for (i = 0; i < len / 2; i++)
+	for (i = 0; i < len1 / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len];
-		s[len--] = tmp;
+		s[i] = s[len2];
+		s[len2--] = tmp;
 	}
+
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,14 @@
 */
 void puts2(char *str)
 {
-i	int len, i;
-
+	int len, i;
 	len = 0;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
-	for (i = 0; i < len - 1; i += 2)
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
