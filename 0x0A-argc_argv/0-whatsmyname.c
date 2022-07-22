4405c3f3482438719dabb754e74f5622dd4c2b0b
Geoffy1 <geojogu@gmail.com>
2022-07-22 14:55:48 -0700

    task 0 to 5

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - prints name
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0forsuccess always
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int coinConverter(int i);
+
+
+
+
+
+#endif
