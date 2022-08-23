3fb03fc12cb797541b2f252c6ab8c55efb7c7e69
Geoffy1 <geojogu@gmail.com>
2022-08-23 03:55:04 -0700

    task advance

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
 #include <unistd.h>
 
 
@@ -13,5 +14,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int main(char argc, int *argv[]);
+int _putchar(char c);
 
 #endif
