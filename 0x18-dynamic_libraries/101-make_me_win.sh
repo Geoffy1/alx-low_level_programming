fa80bc83fde76b42193b1b3e62d9a67f3979c384
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-10-13 02:55:25 -0700

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/Geoffy1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so -P ../
-export LD_PRELOAD="$PWD/../libtest.so"
+wget /tmp/https://raw.githubusercontent.com/Geoffy1/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so -P
+export LD_PRELOAD=/tmp/libtest.so
